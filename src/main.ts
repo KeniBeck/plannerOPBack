a32b8b9a810ec096d64c1d72731fe3dc09f2cda7
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-02-27 17:34:49 -0500

    modified resources, user, worker and pipeline creation parse-int

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,10 @@ async function bootstrap() {
     }),
   );
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Planner API')
+    .setDescription('API app planner operations')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Planner operations')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { WorkerService } from './worker.service';
 import { WorkerController } from './worker.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AreaService } from 'src/area/area.service';
+import { UserService } from 'src/user/user.service';
+
 
 @Module({
   controllers: [WorkerController],
-  providers: [WorkerService, PrismaService],
+  providers: [WorkerService, PrismaService, AreaService, UserService],
 })
 export class WorkerModule {}
